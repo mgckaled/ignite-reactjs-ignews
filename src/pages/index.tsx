ac7f744f6a8124245652a9ce6aaaa5057fe5c127
mgckaled <marcelgckaled@gmail.com>
2022-08-15 21:36:35 -0300

    add header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
-// terminação 'module.css' é um css escoped
-import styles from '../styles/home.module.scss'
+// componente que pode ser colocado em qualquer lugar e ao mesmo tempo ser anexado dentro do "_document". Logo é possível configurar informações de cabeçalho conforme a tela de intereção
+import Head from 'next/head'
 
 export default function Home() {
-	return <h1 className={styles.title}>Hello World!</h1>
+	return (
+		<>
+			<h1>
+				<Head>
+					<title>ig.news</title>
+				</Head>
+				ig.news
+			</h1>
+		</>
+	)
 }
