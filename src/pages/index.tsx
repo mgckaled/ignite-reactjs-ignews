287e6362cad56fc3616358042ff002850703b68b
mgckaled <marcelgckaled@gmail.com>
2022-08-18 09:19:27 -0300

    add Sign in button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,20 @@ import Head from 'next/head'
 export default function Home() {
 	return (
 		<>
-			<h1>
-				<Head>
-					<title>ig.news</title>
-				</Head>
-				ig.news
-			</h1>
+			<Head>
+				<title>Home | ig.news</title>
+			</Head>
+
+			<main>
+				<section>
+					<span>👏 Hey, welcome</span>
+					<h1>
+						News about the <span>React</span> world.
+					</h1>
+				</section>
+
+				<img src="/images/avatar.svg" alt="Girl Coding" />
+			</main>
 		</>
 	)
 }
